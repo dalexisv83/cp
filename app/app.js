ca926c38eb4865b026cf1ec51a316fb3ba8a37c6
David Velasquez <DVelasquez2@directv.com>
2016-11-10 13:38:41 -0800

    Initial Commit: No changes to legacy package compare

---
+++ app/app.js
@@ -0,0 +1,6 @@
+/*jslint unparam: true*/
+(function(angular) {
+    'use strict';
+    angular.module('channelspackages', [])
+}(window.angular));
+/*jslint unparam: false*/
---
+++ controllers/MainCtrl.js
@@ -0,0 +1,28 @@
+(function(angular) {
+    'use strict';
+    angular.module('channelspackages')
+        .controller('MainCtrl', ['$scope', '$location',
+            function($scope, $location) {
+                $scope.goHere = function(here) {
+                    $location.path(here);
+                }
+                $scope.tools = [{
+                    "name": "Package Compare",
+                    "url": "package-compare",
+                    "src": "tools/package_compare/index.htm"
+                }];
+                // $scope.trusted = {};
+                // for (var i=0;i<$scope.tools.length;i++) {
+                //     $scope.trusted[$scope.tools[i].url] = $sce.trustAsResourceUrl($scope.tools[i].src);
+                // }
+                $scope.$watch(function() {
+                    return $location.path();
+                }, function(params) {
+                    $scope.page = params.substring(1);
+                }, true);
+                $scope.$watch('page', function() {
+                    $location.path($scope.page);
+                });
+            }
+        ]);
+}(window.angular));
