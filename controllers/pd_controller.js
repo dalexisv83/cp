c59756b38d6b1ec237b895c1f0da366d9aee9a67
David Velasquez <DVelasquez2@directv.com>
2017-01-19 12:05:17 -0800

    IE fixes

--- controllers/pd_controller.js
+++ controllers/pd_controller.js
@@ -71,7 +71,7 @@ app.controller('PD_Controller',['$scope', '$http', '$location',
             function(params) {
                 if (params == 'package-details') {
                     stopWatching();
-                    $http.get('data/PageListRes.json').then(function successCallback(response) {
+                    $http.get('data/PageListRes.js').then(function successCallback(response) {
                         init(response);
                     }, function errorCallback(response) {
                         $http.get('http://localhost/rover_tools/channelsjs/channels.js').then(function successCallback(response) {
@@ -87,3 +87,48 @@ app.controller('PD_Controller',['$scope', '$http', '$location',
             true
         );
 }]);
+
+// https://tc39.github.io/ecma262/#sec-array.prototype.find
+if (!Array.prototype.find) {
+  Object.defineProperty(Array.prototype, 'find', {
+    value: function(predicate) {
+     // 1. Let O be ? ToObject(this value).
+      if (this == null) {
+        throw new TypeError('"this" is null or not defined');
+      }
+
+      var o = Object(this);
+
+      // 2. Let len be ? ToLength(? Get(O, "length")).
+      var len = o.length >>> 0;
+
+      // 3. If IsCallable(predicate) is false, throw a TypeError exception.
+      if (typeof predicate !== 'function') {
+        throw new TypeError('predicate must be a function');
+      }
+
+      // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
+      var thisArg = arguments[1];
+
+      // 5. Let k be 0.
+      var k = 0;
+
+      // 6. Repeat, while k < len
+      while (k < len) {
+        // a. Let Pk be ! ToString(k).
+        // b. Let kValue be ? Get(O, Pk).
+        // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
+        // d. If testResult is true, return kValue.
+        var kValue = o[k];
+        if (predicate.call(thisArg, kValue, k, o)) {
+          return kValue;
+        }
+        // e. Increase k by 1.
+        k++;
+      }
+
+      // 7. Return undefined.
+      return undefined;
+    }
+  });
+}
