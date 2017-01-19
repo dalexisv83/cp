46fbb86a0563b9977371395d889ca4e8327c2062
David Velasquez <DVelasquez2@directv.com>
2017-01-19 13:44:10 -0800

    Adds: 1)links to package url  2) Package description on hover

--- controllers/cl_controller.js
+++ controllers/cl_controller.js
@@ -262,4 +262,22 @@ app.filter('comReplace', [
             }
         }
     }
-]);
+]).directive('qTip', [function() {
+    return {
+        link: function(scope, elem, attrs) {
+            $(elem).qtip({
+                content: {
+                    text: scope.package.description
+                },
+                position: {
+                    my: 'top right',
+                    at: 'bottom left',
+                    target: this,
+                    adjust: {
+                        y: 50
+                    }
+                }
+            });
+        }
+    };
+}]);
