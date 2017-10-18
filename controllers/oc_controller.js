6dd88fa9b1c51244e6edf1ed8a6b77a8638f6dff
Velasquez, David (dv365y) <dv365y@att.com>
2017-10-18 11:08:17 -0700

    [101817] Implements calculation on button click

--- controllers/oc_controller.js
+++ controllers/oc_controller.js
@@ -22,17 +22,23 @@ app.controller('OC_Controller',['$scope', '$http',
                 $scope.submitted = $scope.current = $scope.requested = $scope.stb_promo = $scope.promo_pricing = $scope.num_rec = $scope.stb_promo = $scope.credit = null;
                 $scope.num_rec = "";
                 $scope.display = false;
+                $scope.invalid = false;
             };
             $scope.receivers = [1, 2, 3, 4, 5, 6];
             $scope.calculate = function () {
-                $scope.grey = false;
-                $scope.cCur = $scope.current;
-                $scope.cReq = $scope.requested;
-                $scope.cDisc = $scope.current[$scope.stb_promo.id].disc;
-                $scope.cRecFee = $scope.num_rec * 7;
-                $scope.cCredit = ($scope.requested[$scope.stb_promo.id].price + ($scope.cDisc || 0)) - ($scope.requested[2].price + ($scope.cDisc || 0) + $scope.cRecFee);
-                $scope.totals = ($scope.requested[2].price + ($scope.cDisc || 0)) + $scope.cRecFee + Math.max(0, $scope.cCredit);
-                $scope.display = true;
+                if ($scope.current && $scope.requested && $scope.num_rec > -1 && $scope.stb_promo && $scope.promo_pricing != undefined) {
+                    $scope.invalid = false;
+                    $scope.grey = false;
+                    $scope.cCur = $scope.current;
+                    $scope.cReq = $scope.requested;
+                    $scope.cDisc = $scope.promo_pricing ? $scope.current[$scope.stb_promo.id].disc : 0;
+                    $scope.cRecFee = $scope.num_rec * 7;
+                    $scope.cCredit = Math.min(0,($scope.requested[$scope.stb_promo.id].price + $scope.cDisc) - ($scope.requested[2].price + $scope.cDisc + $scope.cRecFee));
+                    $scope.totals = ($scope.requested[2].price + $scope.cDisc) + $scope.cRecFee + Math.max(0, $scope.cCredit);
+                    $scope.display = true;
+                } else {
+                    $scope.invalid = true;
+                }
             }
         };
         $http.get('assets/datasource/OfferCalculator.js').then(function successCallback(response) {
